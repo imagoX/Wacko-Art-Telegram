097df51e49cbec56c0cf038b310274eae9454515
imago <mohammad.nakhaei.r3@gmail.com>
2025-04-09 22:38:00 +0330

    bot.yml updated

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-VALID_USERS = [int(user_id) for user_id in os.getenv("VALID_USERS", "").split(",") if user_id]
-CHAT_IDS = [int(chat_id) for chat_id in os.getenv("CHAT_IDS", "").split(",") if chat_id]
+VALID_USERS = [int(user_id) for user_id in os.getenv("TELEGRAM_VALID_USERS", "").split(",") if user_id]
+CHAT_IDS = [int(chat_id) for chat_id in os.getenv("TELEGRAM_CHAT_ID", "").split(",") if chat_id]
