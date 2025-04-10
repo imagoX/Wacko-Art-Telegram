f1b8f489abd7717dd8344d53c6b93fe645eaaa59
imago <139993611+imagoX@users.noreply.github.com>
2025-04-10 10:50:44 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-VALID_USERS = [int(user_id) for user_id in os.getenv("TELEGRAM_VALID_USERS", "").split(",") if user_id]
-CHAT_IDS = [int(chat_id) for chat_id in os.getenv("TELEGRAM_CHAT_ID", "").split(",") if chat_id]
+valid_users_str = os.getenv("TELEGRAM_VALID_USERS", "").replace('[', '').replace(']', '').strip()
+VALID_USERS = [int(user_id) for user_id in valid_users_str.split(",") if user_id]
+chat_ids_str = os.getenv("TELEGRAM_CHAT_ID", "").replace('[', '').replace(']', '').strip()
+CHAT_IDS = [int(chat_id) for chat_id in chat_ids_str.split(",") if chat_id]
